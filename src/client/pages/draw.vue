5958df2ec5dcf814478724c132658aaca5a2d1b3
Дима Винцык <dima.vintsyk@gmail.com>
2020-02-09 19:09:58 +0200

    canvas

---
+++ src/client/pages/draw.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="game-container">
+      <Canvas />
+    </div>
+  </div>
+</template>
+
+<script>
+import Canvas from '~/components/Canvas.vue'
+
+export default {
+    components: {
+        Canvas
+    }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.game-container {
+    width: 900px;
+}
+</style>
