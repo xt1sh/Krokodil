ab27cac973cba174fae2d1e31458f40977822c2b
Дима Винцык <dima.vintsyk@gmail.com>
2020-02-16 11:27:42 +0200

    word picker

---
+++ src/server/Services/WordPicker/IWordPicker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Krokodil.Services.WordPicker
+{
+    public interface IWordPicker
+    {
+        List<string> GetRandomSample(List<string> words, int count);
+
+        void CacheWord(object key, string word, double minutes);
+
+        string GetCachedWord(object key);
+
+        void RemoveCachedWord(object key);
+    }
+}
---
+++ src/server/Services/WordPicker/WordPicker.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Krokodil.Services.WordPicker
+{
+    public class WordPicker : IWordPicker
+    {
+        private IMemoryCache _cache;
+
+        public WordPicker(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+
+        public List<string> GetRandomSample(List<string> words, int count)
+        {
+            var rnd = new Random();
+
+            return words.OrderBy(x => rnd.Next(words.Count)).Take(count).ToList();
+        }
+
+        public void CacheWord(object key, string word, double minutes)
+        {
+            _cache.Set(key, word, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(minutes)
+            });
+        }
+
+        public string GetCachedWord(object key)
+        {
+            string word;
+
+            if (!_cache.TryGetValue(key, out word))
+                return null;
+
+            return word;
+        }   
+
+        public void RemoveCachedWord(object key)
+        {
+            _cache.Remove(key);
+        }
+    }
+}
